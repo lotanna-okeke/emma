acc3d0f49f8120472f6349660eb033198afbe65d
lotanna-okeke <lotanna.okeke@pau.edu.ng>
2026-01-05 16:27:55 +0000

    android

---
+++ android/app/src/main/kotlin/com/bluetagit/college_port/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bluetagit.college_port
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/bluetagit/school_port_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bluetagit.strag
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
